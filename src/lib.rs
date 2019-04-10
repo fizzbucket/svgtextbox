820cf3e41a95cf749a14ef1281a4e09e3b1fbf5e
Tom McLean <thomasowenmclean@gmail.com>
2019-04-10 11:48:40 +0100

    Api changes and tests to replace panics with errors

--- src/lib.rs
+++ src/lib.rs
@@ -52,16 +52,16 @@
 //! ```
 //! extern crate pango;
 //!
-//! let left_aligned = svgtextbox::SVGTextBox::new("Left".to_string(), 100, 100, "Sans 10")
+//! let left_aligned = svgtextbox::SVGTextBox::new("Left".to_string(), 100, 100, "Sans")
 //!							.set_alignment_from_str("left")
 //!							.to_string();
-//! let also_left = svgtextbox::SVGTextBox::new("Left".to_string(), 100, 100, "Sans 10")
+//! let also_left = svgtextbox::SVGTextBox::new("Left".to_string(), 100, 100, "Sans")
 //!							.set_alignment(pango::Alignment::Left)
 //!							.to_string();
-//! let centre_aligned = svgtextbox::SVGTextBox::new("Centre".to_string(), 100, 100, "Sans 10")
+//! let centre_aligned = svgtextbox::SVGTextBox::new("Centre".to_string(), 100, 100, "Sans")
 //!							.set_alignment_from_str("centre")
 //!							.to_string();
-//! let right_aligned = svgtextbox::SVGTextBox::new("Right".to_string(), 100, 100, "Sans 10")
+//! let right_aligned = svgtextbox::SVGTextBox::new("Right".to_string(), 100, 100, "Sans")
 //!							.set_alignment_from_str("right")
 //!							.to_string();
 //! ```
@@ -93,8 +93,8 @@
 //!		For example, as can be seen above, calls to change the font size are passed on to pango; the unit expected there is the size in pts * `pango::SCALE`.
 //! * I'm pretty sure that there _are_ memory leaks. Fixing them is one of the blockages to making this a public crate. If present, they're minor,
 //! 	but still enough using this as a long-running program a bad idea.
-//! * Text will not be set to a base size of more than 1000pts.
-//! 	There's no particular reason for this number, but some limit was required, and that's high enough to not be a problem for almost anything.
+//! * Text will not be set to a base size of more than 500 pts.
+//! 	There's no particular reason for this number, but some limit was required, and that's high enough to not be a problem generally.
 use std::fs;
 use std::str;
 
@@ -103,43 +103,16 @@ use pango::LayoutExt;
 extern crate cairo;
 use cairo::prelude::*;
 extern crate pangocairo;
+extern crate regex;
+use regex::Regex;
 
-
-pub mod utils {
-
-	/// Converts px to the equivalent in pts.
-	/// # Examples
-	///
-	/// ```
-	/// let five_px = 5;
-	/// assert_eq!(3.75, svgtextbox::utils::px_to_pts(five_px));
-	/// ```
-	pub fn px_to_pts(px: i32) -> f64 {
-		px as f64 * 0.75
-	}
-
-	/// Scales n by pango::SCALE.
-	/// # Examples
-	/// ```
-	/// let unscaled_font_size = 10;
-	/// let scaled = svgtextbox::utils::pango_scale(unscaled_font_size);
-	/// assert_eq!(scaled, 10240);
-	/// ```
-	pub fn pango_scale(n: i32) -> i32 {
-		n * pango::SCALE
-	}
-
-	/// Just a wrapper around `pango::FontDescription::from_string` to prevent
-	/// needing to import for callers.
-	/// ```
-	/// let a = svgtextbox::utils::pango_font_description_from_str("Sans 10");
-	/// let b = pango::FontDescription::from_string("Sans 10");
-	/// assert_eq!(a, b)
-	/// ```
-	pub fn pango_font_description_from_str(fd: &str) -> pango::FontDescription {
-		pango::FontDescription::from_string(fd)
-	}
-}
+pub const SCALE: i32 = pango::SCALE;
+pub const MAX_FONT_SIZE: i32 = 500;
+pub const SCALED_MAX_FONT_SIZE: i32 = MAX_FONT_SIZE * SCALE;
+pub const DEFAULT_FONT_SIZE: i32 = 10; // default font size to use if not scaling text.
+pub const PX_TO_PT_RATIO: f32 = 0.75; // the ratio of pixels to points; ie Ypx multiplied by PX_TO_PT_RATIO = Ypts,
+// or, of course, Ypts divided by PX_TO_PT_RATIO = Ypx.
+pub const MAX_MARKUP_LEN: i32 = 1000;
 
 pub struct SVGTextBox {
 	pub markup: String,
@@ -160,9 +133,9 @@ impl SVGTextBox {
 	/// // this sets certain options;
 	/// // note that some are changed:
 	///	assert_eq!(tb.markup, "Hello World");
-	///	assert_eq!(tb.width, svgtextbox::utils::px_to_pts(100) as i32);
-	///	assert_eq!(tb.height, svgtextbox::utils::px_to_pts(100) as i32);
-	///	assert_eq!(tb.font_desc, svgtextbox::utils::pango_font_description_from_str("Sans 12"));
+	///	assert_eq!(tb.width as f32, 100.0 * svgtextbox::PX_TO_PT_RATIO);
+	///	assert_eq!(tb.height as f32, 100.0 * svgtextbox::PX_TO_PT_RATIO);
+	///	assert_eq!(tb.font_desc, pango::FontDescription::from_string("Sans 12"));
 	///	assert_eq!(tb.alignment, None);
 	///	assert_eq!(tb.grow, None);
 	///
@@ -186,19 +159,58 @@ impl SVGTextBox {
 	/// * `height`: the height of the eventual image, in pixels.
 	/// * `font_desc_str`: a string to be passed to `pango::FontDescription::new_from_string` in order to generate a `FontDescription`.
 	///
-	pub fn new(markup: String, width: i32, height: i32, font_desc_str: &str) -> SVGTextBox {
+	pub fn new(markup: String, width: i32, height: i32, font_desc_str: &str) -> Result<SVGTextBox, &str> {
 		
 		// need to operate in pts on an svg surface
-		let pt_width = utils::px_to_pts(width);
-		let pt_height = utils::px_to_pts(height);
-	
-		SVGTextBox {
-			markup: markup,
+		let pt_width = width as f32 * PX_TO_PT_RATIO;
+		let pt_height = height as f32 * PX_TO_PT_RATIO;
+
+		let checked_markup = SVGTextBox::check_markup(markup)?;
+		let checked_font_desc_str = SVGTextBox::check_markup(font_desc_str.to_string())?;
+
+		Ok(SVGTextBox {
+			markup: checked_markup,
 			width: pt_width as i32,
 			height: pt_height as i32,
-			font_desc: utils::pango_font_description_from_str(font_desc_str),
+			font_desc: pango::FontDescription::from_string(&checked_font_desc_str),
 			alignment: None,
 			grow: None
+		})
+	}
+
+	// Pango has a special relationship with ampersands.
+	fn handle_ampersand(mut markup: String) -> Result<String, &'static str> {
+		let unisolated_ampersand = Regex::new(r"&\S+[^;]\s").unwrap();
+		if unisolated_ampersand.is_match(&markup) {
+			return Err("Possible HTML entity not ending with colon.");
+		}
+
+		let isolated_ampersand = Regex::new(r"&(?P<w>\s+)").unwrap();
+		if isolated_ampersand.is_match(&markup) {
+			markup = isolated_ampersand.replace_all(&markup, "&amp;$w").to_string();
+		}
+		Ok(markup)
+	}
+
+	fn check_markup(mut markup: String) -> Result<String, &'static str> {
+		
+		if markup.len() as i32 > MAX_MARKUP_LEN {
+			return Err("Markup is too long.");
+		}
+
+		if markup.contains('\u{0}') {
+			return Err("Unsupported char used.");
+		}
+
+		let has_non_whitespace = markup.chars().any(|c| !c.is_whitespace());
+		if markup.is_empty() | !has_non_whitespace {
+			return Err("Empty markup");
+		}
+
+
+		match markup.contains("&") {
+			false => return Ok(markup),
+			true => return SVGTextBox::handle_ampersand(markup),
 		}
 	}
 
@@ -206,7 +218,8 @@ impl SVGTextBox {
 	/// ```
 	/// let mut tb = svgtextbox::SVGTextBox::new("Hello World".to_string(), 100, 100, "Sans");
 	/// let fd = pango::FontDescription::from_string("Serif");
-	/// tb.set_font_desc(fd);
+	/// tb.set_font_desc(fd.clone());
+	/// assert_eq!(tb.font_desc, fd);
 	/// ```
 	pub fn set_font_desc(&mut self, f: pango::FontDescription) -> &mut SVGTextBox {
 		self.font_desc = f;
@@ -272,14 +285,13 @@ impl SVGTextBox {
 		let pango_context = pangocairo::functions::create_context(context);
 		match pango_context {
 			Some(pango_context) => Ok(pango_context),
-			_ => Err("Could not create pango context.")
+			_ => Err("Could not create context")
 		}
+
 	}
 
 	/// Get a pango layout from the context with all our choices set.
-	/// This is public only to make it possible to give examples for `impl LayoutExtension`
-	/// and probably shouldn't be used directly, really.
-	pub fn get_layout(&self, context: &cairo::Context) -> Result<pango::Layout, &str> {
+	fn get_layout(&self, context: &cairo::Context) -> Result<pango::Layout, &str> {
 		let pango_context = self.get_pango_context(context)?;
 		let layout = pango::Layout::new(&pango_context);
 
@@ -287,11 +299,11 @@ impl SVGTextBox {
 
 		layout.set_markup(&self.markup);
 		layout.set_font_description(&self.font_desc);
-		layout.set_width(utils::pango_scale(self.width));
-	 	layout.set_height(utils::pango_scale(self.height));
-	    layout.set_ellipsize(pango::EllipsizeMode::End);
+		layout.set_ellipsize(pango::EllipsizeMode::End);
+		layout.set_width(self.width * SCALE);
+	 	layout.set_height(self.height * SCALE);
 	    layout.set_alignment(alignment);
-	    layout.set_wrap(pango::WrapMode::Word);
+	    //layout.set_wrap(pango::WrapMode::Word);
 	    Ok(layout)
 	}
 
@@ -306,25 +318,29 @@ impl SVGTextBox {
 		let context = cairo::Context::new(&surface);
 		let layout = self.get_layout(&context)?;
 
-		let grow = self.grow.unwrap_or(true);
-
-	    if grow {
+	    if self.grow.unwrap_or(true) {
 		    let max_font_size = layout.max_font_size();
-		    layout.change_font_size(max_font_size);
+		    let font_change_result = layout.change_font_size(max_font_size);
+		    if (max_font_size == 0) | font_change_result.is_err() | !layout.fits() {
+		    	return Err("Could not fit text at minimum font size");
+		    }
 		} else {
 			// There could be no size at all set.
 			let current_size = layout.get_base_font_size();
 			if current_size == 0 {
-				layout.change_font_size(utils::pango_scale(10));
+				let changed = layout.change_font_size(DEFAULT_FONT_SIZE * SCALE);
+			if changed.is_err() | !layout.fits() {
+				return Err("Could not fit text at default font size.")
+			}
 			}
 		}
 
 		// pts despite function name.
 	    let (_, layout_current_height_pt) = layout.get_pixel_size();
-	    let pts_remaining = self.height as f64 - layout_current_height_pt as f64;
-	    let top_padding_pts = pts_remaining / 2.0;
-
-		context.move_to(0.0, top_padding_pts);
+	    let pts_remaining = self.height - layout_current_height_pt;
+	    let top_padding_pts = pts_remaining / 2;
+		context.move_to(0.0, top_padding_pts as f64);
+		
 		pangocairo::functions::show_layout(&context, &layout);
 	    surface.finish();
 
@@ -367,7 +383,7 @@ impl SVGTextBox {
 
 
 pub trait LayoutExtension {
-	fn change_font_size(&self, new_size: i32);
+	fn change_font_size(&self, new_size: i32) -> Result<(), &str>;
 	fn max_font_size(&self) -> i32;
 	fn get_base_font_size(&self) -> i32;
 	fn fits(&self) -> bool;
@@ -388,70 +404,48 @@ impl LayoutExtension for pango::Layout {
 		}
 		// But Pango's interpretation of this is not ours,
 		// since we're imposing the idea of a bounding box.
-		// Ink extents are the size of things as printed
+		// Ink extents are the size of things as printed,
 		// logical extents are those intended to be used for
 		// positioning. (Think of a `g` extending below baseline.)
 		// I think that these _logical_ extents are what get used
 		// by pango in calculating whether to ellipsize.
-		// But we actually want to be sure that the ink extents, not the
-		// logical extents, don't go beyond the bounds of our box.
-		// making a check on ellipsization something that sometimes
-		// fails: text will be inked beyond the boundaries of the box,
+		// This means a check on ellipsization alone is inadequate:
+		// text will be inked beyond the boundaries of the box,
 		// even if it's not ellipsized.
 		// So we need to check this also.
 		
-		let intended_height =  self.get_height();
+		let intended_height = self.get_height();
 		let intended_width = self.get_width();
 
-		let (ink_extents, logical_extents) = self.get_extents();
+		let (ink_extents, _logical_extents) = self.get_extents();
 
-		let x_negative = ink_extents.x < 0;
-		let y_negative = ink_extents.y < 0;
-
-		if x_negative | y_negative {
+		// First step is to check that ink extents start within bounds.
+		if (ink_extents.x < 0) | (ink_extents.y < 0) {
 			return false;
 		}
-		// this is highly unlikely to be a problem, since ink dimensions 
-		// are always almost less than logical, but might as well check. 
-		let too_high = ink_extents.height > intended_height;
-		let too_wide = ink_extents.width > intended_width;
+
+		// Even if we know ink extents are within bounds on left and top side,
+		// we can also encounter trouble if a distance + start point spills over the bounds
+		// on right or bottom.		
+		let too_high = (ink_extents.height + ink_extents.y) > intended_height;
+		let too_wide = (ink_extents.width + ink_extents.x) > intended_width;
+
 		if too_high | too_wide {
 			return false;
 		}
 
-		// We can also encounter trouble if, for example, the logical height is, when added to
-		// the start point given by ink_extents.y, greater than the total intended height.
-		let total_height = ink_extents.y + logical_extents.height;
-		let total_width = ink_extents.x + logical_extents.width;
-		if (total_height > intended_height) | (total_width > intended_width) {
-			return false;
-		}
 		true
 	}
 
 	/// Change the base font size of this layout.
-	///
-	/// ```
-	/// use pango::LayoutExt;
-	/// use svgtextbox::LayoutExtension;
-	///
-	/// // get a layout
-	/// let tb = svgtextbox::SVGTextBox::new("Hello World".to_string(), 100, 100, "Sans 10");
-	/// let mut writable = Vec::new();
-	/// let surface = cairo::svg::RefWriter::new(tb.width as f64, tb.height as f64, &mut writable);
-	/// let context = cairo::Context::new(&surface);
-	/// let layout = tb.get_layout(&context).unwrap();
-	///
-	/// let original_size = layout.get_base_font_size();
-	/// assert_eq!(original_size, 10 * pango::SCALE);
-	/// layout.change_font_size(11 * pango::SCALE);
-	/// assert!(original_size != layout.get_base_font_size());
-	/// assert_eq!(11 * pango::SCALE, layout.get_base_font_size());
-	/// ```
-	fn change_font_size(&self, new_size: i32) {
+	fn change_font_size(&self, new_size: i32) -> Result<(), &str> {
+		if new_size > SCALED_MAX_FONT_SIZE {
+			return Err("The maximum font size was exceeded.");
+		}
 		let mut font_desc = self.get_font_description().unwrap();
 		font_desc.set_size(new_size);
 		self.set_font_description(&font_desc);
+		Ok(())
 	}
 
 
@@ -459,8 +453,7 @@ impl LayoutExtension for pango::Layout {
 	/// the layout then no longer fits, return 
 	/// Ordering::Greater, otherwise Ordering::Less.
 	fn overflows(&self, n:i32) -> std::cmp::Ordering {
-		let scaled = utils::pango_scale(n);
-		self.change_font_size(scaled);
+		self.change_font_size(n * SCALE).unwrap();
 		if self.fits() {
 			return std::cmp::Ordering::Less;
 		}
@@ -469,50 +462,61 @@ impl LayoutExtension for pango::Layout {
 
 	/// Return the largest base font size which would
 	/// still avoid text not fitting the box.
-	///
-	/// ```
-	/// use pango::LayoutExt;
-	/// use svgtextbox::LayoutExtension;
-	///
-	/// // get a layout
-	/// let tb = svgtextbox::SVGTextBox::new("Hello World".to_string(), 100, 100, "Sans 10");
-	/// let mut writable = Vec::new();
-	/// let surface = cairo::svg::RefWriter::new(tb.width as f64, tb.height as f64, &mut writable);
-	/// let context = cairo::Context::new(&surface);
-	/// let layout = tb.get_layout(&context).unwrap();
-	///
-	/// let max_size = layout.max_font_size();
-	/// // A layout at max_size still fits:
-	/// layout.change_font_size(max_size);
-	/// assert!(layout.fits());
-	/// // But text one pt larger is:
-	/// layout.change_font_size(max_size + pango::SCALE);
-	/// assert!(!layout.fits());
-	/// ```
 	fn max_font_size(&self) -> i32 {
-		let font_pts_vec = (1..1001).collect::<Vec<i32>>();
+
+		// If we don't do this, text after the first \n will sometimes disappear?
+		// TODO: work out why;
+		// doesn't seem to happen in a brute-force search
+		// rather than a binary one. Do changes to
+		// very large sizes lead pango to throw out lines?.
+		self.set_single_paragraph_mode(true);
+
+		let font_pts_vec = (1..=MAX_FONT_SIZE).collect::<Vec<i32>>();
 		let search_result = font_pts_vec.binary_search_by(|i| self.overflows(*i));
-		// This will always be an error,
-		// representing the index of the lowest size that is too big
+		
+
+		// Almost invariably, this will be an error representing the
+		// index of the lowest font size that is too big.
 		let index = search_result.err().unwrap();
-		// therefore:
-		let goldilocks = font_pts_vec[index-1];
-		utils::pango_scale(goldilocks)
+
+		// But in rare cases it will be that the max font size is exceeded.
+
+		if index == font_pts_vec.len() {
+			while !self.fits() {
+				let current_size = self.get_base_font_size();
+				let new_size = current_size + SCALE;
+				self.change_font_size(new_size);
+				if !self.fits() {
+					return current_size;
+				}
+			}
+		}
+
+		// Otherwise, this is an error,
+		// representing the index of the lowest size that is too big
+		let alice = if index > 0 {font_pts_vec[index-1]} else {font_pts_vec[index]};
+
+		//  "All persons more than a mile high to leave the court."
+		let mut rule_42 = alice * SCALE;
+
+		// Now revert back into multiparagraph mode.
+		self.change_font_size(rule_42).unwrap();
+		self.set_single_paragraph_mode(false);
+
+		// Changing to multi-paragraph means we could now be too
+		// large, since we've added new lines.
+		// By only changing size down we seem to avoid the
+		// mysterious disappearance of lines.
+		
+		while !self.fits() {
+			rule_42 = self.get_base_font_size() - SCALE;
+			self.change_font_size(rule_42).unwrap();
+		}
+
+		rule_42
 	}
 
 	/// Get the base font size used in this layout.
-	/// ```
-	/// use pango::LayoutExt;
-	/// use svgtextbox::LayoutExtension;
-	///
-	/// // get a layout
-	/// let tb = svgtextbox::SVGTextBox::new("Hello World".to_string(), 100, 100, "Sans 10");
-	/// let mut writable = Vec::new();
-	/// let surface = cairo::svg::RefWriter::new(tb.width as f64, tb.height as f64, &mut writable);
-	/// let context = cairo::Context::new(&surface);
-	/// let layout = tb.get_layout(&context).unwrap();
-	///
-	/// assert_eq!(layout.get_base_font_size(), 10 * pango::SCALE)
 	fn get_base_font_size(&self) -> i32 {
 		self.get_font_description().unwrap().get_size()
 	}
--- src/tests.rs
+++ src/tests.rs
@@ -1,49 +1,148 @@
 use std::fs;
 use super::*;
-
-use crate::utils::pango_scale;
-use crate::utils::px_to_pts;
-use crate::LayoutExtension;
+use proptest::prelude::*;
 
 
-
-fn example_textbox() -> SVGTextBox {
+fn example_textbox() -> Result<SVGTextBox, &'static str> {
 	SVGTextBox::new("Hello World".to_string(), 100, 100, "Serif 12")
+}
+
+fn layout_from_tb(tb: SVGTextBox) -> pango::Layout {
+	let mut writable = Vec::new();
+	let surface = cairo::svg::RefWriter::new(tb.width as f64, tb.height as f64, &mut writable);
+	let context = cairo::Context::new(&surface);
+	let layout = tb.get_layout(&context).unwrap();
+	layout
 }
 
 #[test]
 fn test_static_text_size() {
-	let eg = example_textbox().static_text_size().grow.unwrap();
+	let eg = example_textbox().unwrap().static_text_size().grow.unwrap();
 	assert_eq!(eg, false);
-	assert_eq!(example_textbox().grow.unwrap_or(true), true)
-
+	assert_eq!(example_textbox().unwrap().grow.unwrap_or(true), true)
 }
 
 #[test]
 fn test_to_file() {
-	let r = example_textbox().to_file("test.svg");
+	let r = example_textbox().unwrap().to_file("test.svg");
 	r.unwrap();
 	fs::remove_file("test.svg").unwrap();
 }
 
 #[test]
-fn check_layout_sizing() {
-	let mut tb = SVGTextBox::new("SOME PUBLISHER".to_string(), 900, 900, "Spectral");
-	tb.set_alignment_from_str("centre");
-	let mut writable = Vec::new();
-	let surface = cairo::svg::RefWriter::new(tb.width as f64, tb.height as f64, &mut writable);
-	let context = cairo::Context::new(&surface);
-	let layout = tb.get_layout(&context).unwrap();
+fn check_get_max_size() {
+	let layout = layout_from_tb(example_textbox().unwrap());
+	let max_size = layout.max_font_size();
+	// A layout at max_size still fits:
+	layout.change_font_size(max_size).unwrap();
+	assert!(layout.fits());
+	// But text one pt larger is too big:
+	layout.change_font_size(max_size + pango::SCALE).unwrap();
+	assert!(!layout.fits());
+}
+
+#[test]
+fn check_layout_change_size() {
+	let layout = layout_from_tb(example_textbox().unwrap());
+	let original_size = layout.get_base_font_size();
+	assert_eq!(original_size, 12 * pango::SCALE);
+	layout.change_font_size(11 * pango::SCALE).unwrap();
+	assert!(original_size != layout.get_base_font_size());
+	assert_eq!(11 * pango::SCALE, layout.get_base_font_size());
+}
+
+
+
+fn _check_layout_sizing(layout: pango::Layout) {
 	let max = layout.max_font_size();
-	layout.change_font_size(max);
-	
+	layout.change_font_size(max).unwrap();
+
+	let extents = layout.get_extents();
+	let (ink_extents, _logical_extents) = extents;
+
+	let (width, height) = layout.get_pixel_size();
+	assert!(width < layout.get_width());
+	assert!(height < layout.get_height());
+
 	assert!(!layout.is_ellipsized());
-	let (ink_extents, logical_extents) = layout.get_extents();
 	assert!(ink_extents.x >= 0);
 	assert!(ink_extents.y >= 0);
 
 	assert!(ink_extents.height <= layout.get_height());
 	assert!(ink_extents.width <= layout.get_width());
-	assert!((ink_extents.x + logical_extents.width) < layout.get_width());
-	assert!((ink_extents.y + logical_extents.height) < layout.get_height());
+	assert!((ink_extents.x + ink_extents.width) < layout.get_width());
+	assert!((ink_extents.y + ink_extents.height) < layout.get_height());
 }
+
+
+#[test]
+fn check_layout_sizing_1() {
+	let mut tb = SVGTextBox::new("SOME PUBLISHER".to_string(), 900, 900, "Spectral").unwrap();
+	tb.set_alignment_from_str("centre");
+	let layout_1 = layout_from_tb(tb);
+	_check_layout_sizing(layout_1);
+}
+
+#[test]
+fn check_layout_sizing_2() {
+	let mut tb2 = SVGTextBox::new("SOME BOOK\n――\nSOME MANY NAMED AUTHOR".to_string(), 2000, 1200, "Spectral").unwrap();
+	tb2.set_alignment_from_str("centre");
+	let layout = layout_from_tb(tb2);
+	_check_layout_sizing(layout);
+
+}
+
+#[test]
+#[should_panic(expected = "Possible HTML entity not ending with colon.")]
+fn test_bad_ampersands_in_markup() {
+	SVGTextBox::new("Trouble &amp Strife".to_string(), 1000, 1000, "Serif 12").unwrap().to_bytes();
+}
+
+#[test]
+fn test_escaped_ampersand_in_markup() {
+	let tb = SVGTextBox::new("Trouble &amp; Strife".to_string(), 1000, 1000, "Serif 12").unwrap();
+	assert_eq!(tb.markup, "Trouble &amp; Strife".to_string());
+}
+
+#[test]
+fn test_unescaped_ampersand_in_markup() {
+	let tb2 = SVGTextBox::new("Trouble & Strife".to_string(), 1000, 1000, "Serif 12").unwrap();
+	assert_eq!(&tb2.markup, "Trouble &amp; Strife");
+}
+
+
+#[test]
+fn test_dodgy_markup() {
+	let r = SVGTextBox::new("\\".to_string(), 1000, 1000, "Serif 12");
+	if !r.is_err() {
+		let x = r.unwrap();
+		x.to_bytes();
+	}
+	let e = SVGTextBox::new("Hello \u{0}".to_string(), 1000, 1000, "Serif 12");
+	if !e.is_err() {
+		let y = e.unwrap();
+		y.to_bytes();
+	}
+
+}
+
+
+proptest! {
+    #[test]
+    fn no_crash(s in "[^&]") {
+        let r = SVGTextBox::new(s, 1000, 1000, "Serif 12");
+        if !r.is_err() {
+        	let x = r.unwrap().to_bytes();
+        }
+    }
+
+    #[test]
+    fn no_crash_2(s in "[^&]") {
+        let r = SVGTextBox::new("Hello World".to_string(), 1000, 1000, &s);
+        if !r.is_err() {
+        	let x = r.unwrap().to_bytes();
+        }
+    }
+}
+
+
